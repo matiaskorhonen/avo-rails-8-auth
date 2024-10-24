b056c74053f11ee7ccf890b4c70caed9190bf4b5
Matias Korhonen <me@matiaskorhonen.fi>
2024-10-24 19:44:39 +0300

    Set-up authentication
    `bin/rails generate authentication`

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   mount Avo::Engine, at: Avo.configuration.root_path
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
